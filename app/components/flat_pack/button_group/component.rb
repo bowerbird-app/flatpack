194f823ecdf8bfff73a2149fdf249b9808e0755a
Lim Choon Yun (Marco) <marco.cylim@gmail.com>
2026-01-22 06:33:31 +0000

    Add alignment icons for button group examples

--- app/components/flat_pack/button_group/component.rb
+++ app/components/flat_pack/button_group/component.rb
@@ -3,22 +3,10 @@
 module FlatPack
   module ButtonGroup
     class Component < FlatPack::BaseComponent
+      renders_many :buttons, ->(component) { component }
+
       def initialize(**system_arguments)
         super
-        @buttons = []
-      end
-
-      def with_button(component)
-        # Collects a button component to be rendered within the group.
-        # Supports method chaining to allow fluent API style.
-        @buttons << component
-        self
-      end
-
-      def call
-        content_tag(:div, **group_attributes) do
-          safe_join(@buttons.map(&:call))
-        end
       end
 
       private
