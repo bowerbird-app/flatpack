c5c637522afbaa99ab3bb3322e727869c510bded
Lim Choon Yun (Marco) <marco@bowerbird.io>
2026-03-12 06:28:54 +0000

    style: rubocop auto-correct migration whitespace

--- test/dummy/db/migrate/20260312000000_create_articles.rb
+++ test/dummy/db/migrate/20260312000000_create_articles.rb
@@ -3,9 +3,9 @@
 class CreateArticles < ActiveRecord::Migration[8.0]
   def change
     create_table :articles do |t|
-      t.string  :title,       null: false
-      t.text    :body
-      t.string  :body_format, default: "html", null: false
+      t.string :title, null: false
+      t.text :body
+      t.string :body_format, default: "html", null: false
       t.timestamps
     end
   end
