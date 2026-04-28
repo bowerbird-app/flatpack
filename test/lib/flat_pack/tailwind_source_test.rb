d3f16ea8e688aa35b64ce41025967b6d9a28b407
Lim Choon Yun (Marco) <marco@bowerbird.io>
2026-04-28 05:47:40 +0000

    Revert "Fix dummy app Tailwind token resolution"
    This reverts commit a12655ec84e06e40f2bfc224881684e15f619aba.

--- test/lib/flat_pack/tailwind_source_test.rb
+++ test/lib/flat_pack/tailwind_source_test.rb
@@ -12,12 +12,5 @@ module FlatPack
 
       assert_no_match SELF_REFERENTIAL_VARIABLE_PATTERN, content
     end
-
-    test "dummy tailwind source avoids self referential css variable mappings" do
-      source_path = Rails.root.join("app/assets/stylesheets/application.tailwind.css")
-      content = source_path.read
-
-      assert_no_match SELF_REFERENTIAL_VARIABLE_PATTERN, content
-    end
   end
 end
