c6aab2e18b9493c1e204e4ed7295dd04cb772b27
Lim Choon Yun (Marco) <marco@bowerbird.io>
2026-05-27 05:09:27 +0000

    Fix warning border test expectations and install contract version

--- test/components/flat_pack/text_area_component_test.rb
+++ test/components/flat_pack/text_area_component_test.rb
@@ -170,7 +170,7 @@ module FlatPack
         render_inline(Component.new(name: "description", error: "Invalid"))
 
         html = page.native.to_html
-        assert_includes html, "border-warning"
+        assert_includes html, "border-[var(--color-warning)]"
       end
 
       def test_renders_with_custom_class
